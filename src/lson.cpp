d3ff7b0ccad4e9aa30eb75e212525c0b47c2472d
Steve Hollasch <steve@hollasch.net>
2018-09-16 14:38:47 -0700

    Starting in on C++ code for implementation
    - Just uses `nmake` makefile for now, to be CMake later.
    - Single file for now, later header + lib & command-line utility.

---
+++ src/lson.cpp
@@ -0,0 +1,16 @@
+//==================================================================================================
+// lson.cpp
+//
+// This is the first draft of an LSON lexer/parser. Eventually it will grow into a C++ library and a
+// command-line utility.
+//==================================================================================================
+
+#include <stdio.h>
+
+
+int main ()
+{
+    printf ("Hello world.\n");
+
+    return 0;
+}
